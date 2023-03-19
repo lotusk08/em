4ba9e110486d154ec9d6bb271cfcb2988fcdb4c7
Steve Hoàng <82992510+lotusk08@users.noreply.github.com>
2023-03-19 20:58:38 +0700

    update

---
+++ server/server.js
@@ -0,0 +1,273 @@
+const express = require("express");
+const opn = require("opn");
+const bodyParser = require("body-parser");
+const path = require("path");
+const chokidar = require("chokidar");
+const cfg = require("./config");
+
+const {
+  loadXML,
+  loadTempData,
+  writeXML,
+  saveDataFile,
+  shuffle,
+  saveErrorDataFile
+} = require("./help");
+
+let app = express(),
+  router = express.Router(),
+  cwd = process.cwd(),
+  dataBath = __dirname,
+  port = 8090,
+  curData = {},
+  luckyData = {},
+  errorData = [],
+  defaultType = cfg.prizes[0]["type"],
+  defaultPage = `default data`;
+
+//这里指定参数使用 json 格式
+app.use(
+  bodyParser.json({
+    limit: "1mb"
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+if (process.argv.length > 2) {
+  port = process.argv[2];
+}
+
+app.use(express.static(cwd));
+
+//请求地址为空，默认重定向到index.html文件
+app.get("/", (req, res) => {
+  res.redirect(301, "index.html");
+});
+
+//Set cross -domain access
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+app.post("*", (req, res, next) => {
+  log(`request content：${JSON.stringify(req.path, 2)}`);
+  next();
+});
+
+// Get the data previously set up
+router.post("/getTempData", (req, res, next) => {
+  getLeftUsers();
+  res.json({
+    cfgData: cfg,
+    leftUsers: curData.leftUsers,
+    luckyData: luckyData
+  });
+});
+
+// Get all users
+router.post("/reset", (req, res, next) => {
+  luckyData = {};
+  errorData = [];
+  log(`Reset data successfully`);
+  saveErrorDataFile(errorData);
+  return saveDataFile(luckyData).then(data => {
+    res.json({
+      type: "success"
+    });
+  });
+});
+
+// Get all users
+router.post("/getUsers", (req, res, next) => {
+  res.json(curData.users);
+  log(`Successfully return to draw user data`);
+});
+
+// Get prize information
+router.post("/getPrizes", (req, res, next) => {
+  // res.json(curData.prize);
+  log(`Successfully return to prize data`);
+});
+
+// Save the lucky draw data
+router.post("/saveData", (req, res, next) => {
+  let data = req.body;
+  setLucky(data.type, data.data)
+    .then(t => {
+      res.json({
+        type: "Successfully set！"
+      });
+      log(`Successfully preserved prize data data`);
+    })
+    .catch(data => {
+      res.json({
+        type: "Setup failed！"
+      });
+      log(`Save the prize data failed`);
+    });
+});
+
+// Save the Lucky draw data
+router.post("/errorData", (req, res, next) => {
+  let data = req.body;
+  setErrorData(data.data)
+    .then(t => {
+      res.json({
+        type: "Successfully set！"
+      });
+      log(`The data of the data is successful, the data is successful`);
+    })
+    .catch(data => {
+      res.json({
+        type: "Setup failed！"
+      });
+      log(`The data of the person who did not come, the data failed`);
+    });
+});
+
+// Save the data to the excel
+router.post("/export", (req, res, next) => {
+  let type = [1, 2, 3, 4, 5, defaultType],
+    outData = [["Mã nhân sự", "Họ Tên", "Bộ Phận"]];
+  cfg.prizes.forEach(item => {
+    outData.push([item.text]);
+    outData = outData.concat(luckyData[item.type] || []);
+  });
+
+  writeXML(outData, "/Ket qua quay lucky draw.xlsx")
+    .then(dt => {
+      // res.download('/Ket qua quay li xi.xlsx');
+      res.status(200).json({
+        type: "success",
+        url: "Ket qua quay lucky draw.xlsx"
+      });
+      log(`Export data successfully！`);
+    })
+    .catch(err => {
+      res.json({
+        type: "error",
+        error: err.error
+      });
+      log(`Failed to export data！`);
+    });
+});
+
+//For the unsatisfactory path or request, return to the default page
+//Differential requests return different pages content
+router.all("*", (req, res) => {
+  if (req.method.toLowerCase() === "get") {
+    if (/\.(html|htm)/.test(req.originalUrl)) {
+      res.set("Content-Type", "text/html");
+      res.send(defaultPage);
+    } else {
+      res.status(404).end();
+    }
+  } else if (req.method.toLowerCase() === "post") {
+    let postBackData = {
+      error: "empty"
+    };
+    res.send(JSON.stringify(postBackData));
+  }
+});
+
+function log(text) {
+  global.console.log(text);
+  global.console.log("-----------------------------------------------");
+}
+
+function setLucky(type, data) {
+  if (luckyData[type]) {
+    luckyData[type] = luckyData[type].concat(data);
+  } else {
+    luckyData[type] = Array.isArray(data) ? data : [data];
+  }
+
+  return saveDataFile(luckyData);
+}
+
+function setErrorData(data) {
+  errorData = errorData.concat(data);
+
+  return saveErrorDataFile(errorData);
+}
+
+app.use(router);
+
+function loadData() {
+  console.log("Load Excel data file");
+  let cfgData = {};
+
+  // curData.users = loadXML(path.join(cwd, "data/users.xlsx"));
+  curData.users = loadXML(path.join(dataBath, "data/users.xlsx"));
+  // reshuffle the card
+  shuffle(curData.users);
+
+  // read extracted results
+  loadTempData()
+    .then(data => {
+      luckyData = data[0];
+      errorData = data[1];
+    })
+    .catch(data => {
+      curData.leftUsers = Object.assign([], curData.users);
+    });
+}
+
+function getLeftUsers() {
+  //  Record the currently extracted users
+  let lotteredUser = {};
+  for (let key in luckyData) {
+    let luckys = luckyData[key];
+    luckys.forEach(item => {
+      lotteredUser[item[0]] = true;
+    });
+  }
+  // Records are currently extracted but not online
+  errorData.forEach(item => {
+    lotteredUser[item[0]] = true;
+  });
+
+  let leftUsers = Object.assign([], curData.users);
+  leftUsers = leftUsers.filter(user => {
+    return !lotteredUser[user[0]];
+  });
+  curData.leftUsers = leftUsers;
+}
+
+loadData();
+
+module.exports = {
+  run: function(devPort, noOpen) {
+    let openBrowser = true;
+    if (process.argv.length > 3) {
+      if (process.argv[3] && (process.argv[3] + "").toLowerCase() === "n") {
+        openBrowser = false;
+      }
+    }
+
+    if (noOpen) {
+      openBrowser = noOpen !== "n";
+    }
+
+    if (devPort) {
+      port = devPort;
+    }
+
+    let server = app.listen(port, () => {
+      let host = server.address().address;
+      let port = server.address().port;
+      global.console.log(`lottery server listenig at http://${host}:${port}`);
+      openBrowser && opn(`http://127.0.0.1:${port}`);
+    });
+  }
+};
